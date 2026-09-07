66b5f034b95af4d04903c55bb95f56cd168f45be
Illia Supruniuk <ineskazu2@gmail.com>
2026-09-07 19:32:27 +0300

    Add files via upload

---
+++ script.py
@@ -0,0 +1,26 @@
+a = 10          # int
+b = 3.14        # float
+c = "Привіт"    # str
+d = True        # bool
+e = [1, 2, 3]   # list
+f = (1, 2, 3)   # tuple
+g = {1, 2, 3}   # set
+h = {"x": 1}    # dict
+i = 3           # int
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+print(type(f))
+print(type(g))
+print(type(h))
+print(a + i)
+print(a - i)
+print(a * i)
+print(a / i)
+print(a // i)
+print(a % i)
+print(a ** i)
+print('hello world')
